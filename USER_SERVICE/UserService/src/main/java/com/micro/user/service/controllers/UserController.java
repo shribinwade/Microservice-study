d8ee9e7301fc69f927aa81c0a2a3a31bd6de0216
shrihari <shribinwade.9103@gmail.com>
2024-01-23 21:35:15 +0530

    CircuitBreaker added Resiliance4j

--- USER_SERVICE/UserService/src/main/java/com/micro/user/service/controllers/UserController.java
+++ USER_SERVICE/UserService/src/main/java/com/micro/user/service/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.micro.user.service.controllers;
 
 import com.micro.user.service.entities.User;
 import com.micro.user.service.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -24,9 +27,23 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //creating fall back method for circuitbreaker
+
+    public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
+       log.info("Fallback is executed because service is down : ", ex.getMessage());
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("Created bcz some service is down")
+                .userId("12345")
+                .build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
 
